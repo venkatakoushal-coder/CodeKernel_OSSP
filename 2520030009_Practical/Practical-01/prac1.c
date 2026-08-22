f6cdbfdf520ec80d752f041d08b4f95fa137cded
venkatakoushal-coder <venkatakoushal@klh.edu.in>
2026-08-22 09:22:23 +0530

    Add files via upload

---
+++ 2520030009_Practical/Practical-01/prac1.c
@@ -0,0 +1,28 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/wait.h>
+int main()
+{
+char command[50];
+int pid;
+scanf("%s", command);
+pid=fork();
+if(pid<0)
+{
+printf("Fork failed\n");
+return 1;
+}
+
+if(pid==0)
+{
+printf("child pid: %d\n", getpid());
+printf("parent pid: %d\n",getppid());
+execlp(command,command,NULL);
+}
+else
+{
+printf("current pid: %d\n", getpid());
+printf("parent pid: %d\n", getppid());
+wait(NULL);
+}
+}
