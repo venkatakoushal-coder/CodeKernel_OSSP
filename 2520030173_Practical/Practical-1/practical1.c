3141018463ff0eacc01f4486755597002f3c4346
charansai-8 <yerrapragadacharansai@gmail.com>
2026-08-22 15:51:00 +0530

    Add practical1.c for process creation demonstration

---
+++ 2520030173_Practical/Practical-1/practical1.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    char command[50];
+    int pid;
+
+    scanf("%s", command);
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Fork failed\n");
+        return 1;
+    }
+
+    if (pid == 0)
+    {
+        printf("child pid: %d\n", getpid());
+        printf("parent pid: %d\n", getppid());
+
+        execlp(command, command, NULL);
+    }
+    else
+    {
+        printf("current pid: %d\n", getpid());
+        printf("parent pid: %d\n", getppid());
+    }
+
+    return 0;
+}
