15c63854d8546af92ca30594ae0f86e67ba9e376
venkatakoushal-coder <venkatakoushal@klh.edu.in>
2026-09-14 16:40:25 +0530

    Add zombie process example in zombie.c
    Implement basic fork example with parent and child processes.

---
+++ 2520030009_Practical/Practical-04/zombie.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+{
+int pid;
+pid=fork();
+if(pid==0){
+printf("Child process %d\n",getpid());
+exit(0);
+}
+else{
+printf("Parent process %d\n",getpid());
+sleep(30);
+//wait(NULL);
+printf("Parent process completed");
+}
+}
